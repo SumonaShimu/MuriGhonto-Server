0e5b4788c56de1597575f927ae8db80cf73dc140
Sumona Akter Shimu <sumona59shimu@gmail.com>
2023-05-03 15:15:18 +0600

    server data route added

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
+app.use(cors())
 const port = process.env.PORT || 5000;
+
+const chefs=require('./data/chefs.json')
+const recepies=require('./data/recepies.json')
 
 app.get('/', (req, res) => {
   res.send('Hello from server of Murighonto!')
+})
+app.get('/chefs', (req, res) => {
+  res.send(chefs);
+})
+app.get('/recepies', (req, res) => {
+  res.send(recepies);
 })
 
 app.listen(port, () => {
